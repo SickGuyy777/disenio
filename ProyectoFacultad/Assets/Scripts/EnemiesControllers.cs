7077dd6dd094357f9d76aca9abf4261d3a202299
SickGuyy777 <57241415+KAWkkkk@users.noreply.github.com>
2022-05-31 13:53:48 -0300

    Trabaje en el enemigo
    Que flipee a ver al jugador

--- ProyectoFacultad/Assets/Scripts/EnemiesControllers.cs
+++ ProyectoFacultad/Assets/Scripts/EnemiesControllers.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class EnemiesControllers : MonoBehaviour
 {
-    public float health;
+    public float health, range;
     public float maxHealth = 4;
     public AudioSource sonidodaño;
     public float attackDamage = 1f;
 
+    public Transform player;
+    private float _distToPlayer;
+
     private void Start()
     {
         health = maxHealth;
+    }
+
+    private void Update()
+    {
+        _distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (_distToPlayer <= range)
+        {
+            if (player.position.x > transform.position.x && transform.localScale.x < 0 
+                || player.position.x < transform.position.x && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+        }
     }
 
     public void TakeDamage(float damageAmount)
@@ -22,5 +39,10 @@ public class EnemiesControllers : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void Flip()
+    {
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
     }
 }
